a216ecac614f89135ecf62dc2afb396096d460fd
cfphillips2 <36976465+cfphillips2@users.noreply.github.com>
2019-09-16 18:50:16 -0500

    All tests failing
    Example of failing tests, debugging

--- store/discounts.py
+++ store/discounts.py
@@ -1,11 +1,41 @@
 """Super awesome couopon deals"""
 
 def calculate_order(price, cash_coupon, percent_coupon):
-    pass
+    subtotal = 0
+    total = 0
+    shipping = 0
+    price = float(price)
+    cash_coupon = float(cash_coupon)
+    percent_coupon = float(percent_coupon)
+    tax = 0.06
+    if not price < cash_coupon:
+        if cash_coupon is not None:
+            if percent_coupon is not None:
+                discount_percent = price * (percent_coupon * .01)
+                subtotal = (price - cash_coupon) - discount_percent
+            else:
+                subtotal = (price - cash_coupon)
+        else:
+            subtotal = price
+    else:
+        subtotal = 0
+
+    if price <= 10:
+        shipping = 5.95
+    elif price <= 30:
+        shipping = 7.95
+    elif price <= 50:
+        shipping = 11.95
+    elif price > 50:
+        shipping = 0
+
+    total = subtotal + shipping
+    final_total = total + (total * tax)
+    return final_total
 
 
 if __name__ == "__main__":
-    purchase_amt = input("How much was your purchase: ")
+    cash = input("How much was your purchase: ")
     cash_coupon = input("How much was your cash coupon: ")
     percent_coupon = input("How much was your percent coupon:")
-
+    print("Your total is " + calculate_order(cash, cash_coupon, percent_coupon).__str__())
--- test/test.py
+++ test/test.py
@@ -3,10 +3,48 @@ from store import discounts
 
 class test_case(unittest.TestCase):
     def test_under_10(self):
-        self.assertLess(10, discounts.calculate_order(9, 5, 10))
-        self.assertLess(10, discounts.calculate_order(9, 5, 15))
-        self.assertLess(10, discounts.calculate_order(9, 5, 20))
-        self.assertLess(10, discounts.calculate_order(9, 10, 10))
+        self.assertLess(discounts.calculate_order(9, 5, 10), 10)
+        self.assertLess(discounts.calculate_order(9, 5, 15), 10)
+        self.assertLess(discounts.calculate_order(9, 5, 20), 10)
+        self.assertLess(discounts.calculate_order(9, 10, 10), 10)
+
+    def test_10_lessthan_30(self):
+        self.assertLess(discounts.calculate_order(25, 5, 10), 10)
+        self.assertLess(discounts.calculate_order(25, 5, 15), 10)
+        self.assertLess(discounts.calculate_order(25, 5, 20), 10)
+        self.assertLess(discounts.calculate_order(25, 10, 10), 10)
+        self.assertLess(discounts.calculate_order(25, 10, 20), 10)
+        self.assertLess(discounts.calculate_order(24, 20, 10), 10)
+
+    def test_10_lessthan_50(self):
+        self.assertLess(discounts.calculate_order(49, 5, 10), 10)
+        self.assertLess(discounts.calculate_order(49, 10, 15), 10)
+        self.assertLess(discounts.calculate_order(49, 10, 20), 10)
+        self.assertLess(discounts.calculate_order(49, 10, 25), 10)
+        self.assertLess(discounts.calculate_order(49, 15, 10), 10)
+        self.assertLess(discounts.calculate_order(49, 15, 15), 10)
+        self.assertLess(discounts.calculate_order(49, 15, 25), 10)
+        self.assertLess(discounts.calculate_order(49, 25, 10), 10)
+        self.assertLess(discounts.calculate_order(49, 25, 15), 10)
+        self.assertLess(discounts.calculate_order(49, 25, 25), 10)
+        self.assertLess(discounts.calculate_order(49, 10, 25), 10)
+
+    def test_50_and_above(self):
+        self.assertGreaterEqual(discounts.calculate_order(85, 5, 10), 100)
+        self.assertGreaterEqual(discounts.calculate_order(85, 10, 10), 100)
+        self.assertGreaterEqual(discounts.calculate_order(85, 10, 15), 100)
+        self.assertGreaterEqual(discounts.calculate_order(85, 10, 20), 100)
+        self.assertGreaterEqual(discounts.calculate_order(85, 10, 25), 100)
+        self.assertGreaterEqual(discounts.calculate_order(85, 15, 5), 100)
+        self.assertGreaterEqual(discounts.calculate_order(85, 15, 10), 100)
+        self.assertGreaterEqual(discounts.calculate_order(85, 15, 15), 100)
+        self.assertGreaterEqual(discounts.calculate_order(85, 15, 25), 100)
+        self.assertGreaterEqual(discounts.calculate_order(85, 25, 5), 100)
+        self.assertGreaterEqual(discounts.calculate_order(85, 25, 10), 100)
+        self.assertGreaterEqual(discounts.calculate_order(85, 25, 15), 100)
+        self.assertGreaterEqual(discounts.calculate_order(85, 25, 20), 100)
+        self.assertGreaterEqual(discounts.calculate_order(85, 25, 25), 100)
+
 
 if __name__ == "__main__":
     unittest.main()
